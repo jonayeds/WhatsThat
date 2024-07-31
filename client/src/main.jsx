c38835d0bdb3c8a872f893e531176b49864562d4
jonayeds <jonayeeds@gmail.com>
2024-07-31 14:02:27 +0600

    added add friend style

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,18 +1,17 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import {CssBaseline} from '@mui/material'
-import {HelmetProvider} from 'react-helmet-async'
-import { Toaster } from 'react-hot-toast'
-ReactDOM.createRoot(document.getElementById('root')).render(
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App.jsx";
+import { CssBaseline } from "@mui/material";
+import { HelmetProvider } from "react-helmet-async";
+import { Toaster } from "react-hot-toast";
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <HelmetProvider>
-    <Toaster />
-    <CssBaseline />
-    <div>
-    <App />
-    </div>
+      <Toaster />
+      <CssBaseline />
+      <div onContextMenu={(e) => e.preventDefault()}>
+        <App />
+      </div>
     </HelmetProvider>
-  </React.StrictMode>,
-)
-// onContextMenu={e=>e.preventDefault()}
+  </React.StrictMode>  
+);
