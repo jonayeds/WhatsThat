e75625502739cc79be4abac929c85e1fca1ea202
jonayeds <jonayeeds@gmail.com>
2024-08-01 16:06:21 +0600

    added home component style

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,24 @@
 /* eslint-disable react-refresh/only-export-components */
+import { Box, Typography } from "@mui/material";
 import AppLayout from "../components/layout/AppLayout";
 
 const Home = () => {
     return (
-        <div>
-            Home
-        </div>
+        <Box sx={{
+            display:"flex",
+            alignItems: "center",
+            justifyContent:"center",
+            width:"100%",
+            height:"100%",
+            borderLeft:{
+                xs: "0px",
+                sm: "5px solid #b8c7b7"
+            }
+        }}>
+            <Typography  variant="h5" color={"#778c74"}> 
+            Find a friend to chat
+        </Typography>
+        </Box>
     );
 };
 
